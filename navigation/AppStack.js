d24e3f47062d99f107c2e298992472192c85b840
Riyaj Patel <riyajpatel041@gmail.com>
2021-10-23 18:29:24 +0530

    first commit

---
+++ navigation/AppStack.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import {View, TouchableOpacity,Image, Text} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import EditProfileScreen from '../screens/EditProfileScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const FeedStack = ({navigation}) => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="R2SP"
+      component={HomeScreen}
+      options={{
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: '#2e64e5',
+          fontFamily: 'Kufam-SemiBoldItalic',
+          fontSize: 18,
+        },
+        headerStyle: {
+          shadowColor: '#fff',
+          elevation: 0,
+        },
+        headerRight: () => (
+          <View style={{marginRight: 10}}>
+            <FontAwesome5.Button
+              name="plus"
+              size={22}
+              backgroundColor="#fff"
+              color="#2e64e5"
+              
+            />
+          </View>
+        ),
+        headerLeft: () => (
+          <View style={{marginRight: 10}}>
+            {/* <FontAwesome5.Button
+              name="plus"
+              size={22}
+              backgroundColor="#fff"
+              color="#2e64e5"
+              onPress={() => navigation.navigate('AddPost')}
+            /> */}
+             <Image
+        // source={require('../assets/rn-social-logo.png')}
+        source={require('../assets/20211016_182723_0000.png')}
+        style={{height:80,width:80,backgroundColor:'white'}}
+      />
+          </View>
+        ),
+      }}
+    />
+    
+    <Stack.Screen
+      name="HomeProfile"
+      component={ProfileScreen}
+      options={{
+        title: '',
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#fff',
+          shadowColor: '#fff',
+          elevation: 0,
+        },
+        headerBackTitleVisible: false,
+        headerBackImage: () => (
+          <View style={{marginLeft: 15}}>
+            <Ionicons name="arrow-back" size={25} color="#2e64e5" />
+          </View>
+        ),
+      }}
+    />
+  </Stack.Navigator>
+);
+
+
+const ProfileStack = ({navigation}) => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <Stack.Screen
+      name="EditProfile"
+      component={EditProfileScreen}
+      options={{
+        headerTitle: 'Edit Profile',
+        headerBackTitleVisible: false,
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#fff',
+          shadowColor: '#fff',
+          elevation: 0,
+        },
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const AppStack = () => {
+  const getTabBarVisibility = (route) => {
+    const routeName = route.state
+      ? route.state.routes[route.state.index].name
+      : '';
+
+    if (routeName === 'Chat') {
+      return false;
+    }
+    return true;
+  };
+
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#2e64e5',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={FeedStack}
+        options={({route}) => ({
+          tabBarLabel: 'Home',
+          // tabBarVisible: route.state && route.state.index === 0,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="home-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        })}
+      />
+      {/* <Tab.Screen
+        name="Messages"
+        component={MessageStack}
+        options={({route}) => ({
+          tabBarVisible: getTabBarVisibility(route),
+          // Or Hide tabbar when push!
+          // https://github.com/react-navigation/react-navigation/issues/7677
+          // tabBarVisible: route.state && route.state.index === 0,
+          // tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons
+              name="chatbox-ellipses-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        })}
+      /> */}
+      <Tab.Screen
+        name="Profile"
+        component={ProfileStack}
+        options={{
+          // tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="person-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default AppStack;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  Alert,
+} from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+ return(
+   <Text>Home</Text>
+  );
+};
+
+export default HomeScreen;
